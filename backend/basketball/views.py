43220eda3f1dfd191983b94cfe8af295205ff2d1
Daniel Roque <droque6@icloud.com>
2024-06-08 16:34:22 -0500

    frontend and backend commit

--- backend/basketball/views.py
+++ backend/basketball/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
 import xlsxwriter
@@ -8,6 +8,14 @@ from pathlib import Path
 from datetime import datetime
 from io import StringIO
 
+courts = ["PSA - McKinney 1", "PSA - McKinney 2", "PSA - McKinney 3", "PSA - McKinney 4",
+          "PSA - McKinney 5", "PSA - McKinney 6", "PSA - McKinney 7", "PSA - McKinney 8",
+          "PSA - Murphy 1", "PSA - Murphy 2", "PSA - Murphy 3", "PSA - Murphy 4",
+          "PSA - Murphy 5", "PSA - Murphy 6", "PSA - Murphy 7", "PSA - Murphy 8",
+          "PSA 1 - Blue 1", "PSA 1 - Blue 2", "PSA 1 - Blue 3", "PSA 1 - Blue 4",
+          "PSA 1 - Red 4", "PSA 1 - Red 5", "PSA 1 - Green 1",
+          "PSA 2 - Silver 1", "PSA 2 - Silver 2", "PSA 2 - Silver 3", "PSA 2 - Silver 4",
+          "PSA 2 - Silver 5", "PSA 2 - Silver 6", "PSA 2 - Silver 7", "PSA 2 - Silver 8"]
 
 @csrf_exempt
 def basketball(request):
@@ -28,25 +36,9 @@ def basketball(request):
             date = datetime.strptime(date_str, '%Y-%m-%d').strftime('%Y%m%d')
             
             # Process data for each court and save to Excel
-            with pd.ExcelWriter(os.path.join(downloads_folder, f'sorted_basketball_games{date}.xlsx'), engine='xlsxwriter') as writer:
+            with pd.ExcelWriter(os.path.join(downloads_folder, f'sorted_basketball_games_{date}.xlsx'), engine='xlsxwriter') as writer:
                 for court in courts:
-                    data_dict = {
-                        "Date": data["Date"].to_list(),
-                        "Start Time": data["Start Time"].to_list(),
-                        "Venue": data["Venue"].to_list(),
-                        "League": data["League"].to_list(),
-                        "Home Team": data["Home Team"].to_list(),
-                        "Away Team": data["Away Team"].to_list()
-                    }
-                    
-                    df = pd.DataFrame(data_dict)
-                    df.insert(5, "Home Score", "")
-                    df.insert(7, "Away Score", "")
-                    df.insert(8, "Referee Signature", "")
-                    filtered_data = df[df['Venue'] == court]
-                    court_sorted_data = filtered_data.sort_values(by=["Venue", "Date"])
-                    final_sort = court_sorted_data.drop(columns=['Venue', 'Date'])
-
+                    sorted_data = process_data(data, court)
                     worksheet_name = court.split(" - ")[-1]
                     workbook = writer.book
                     worksheet = workbook.add_worksheet(worksheet_name)
@@ -59,7 +51,7 @@ def basketball(request):
                     worksheet.merge_range('A1:C1', court, workbook.add_format({'bold': True, 'align': 'center'}))
                     worksheet.merge_range('E1:G1', date_str, workbook.add_format({'bold': True, 'align': 'center'}))
 
-                    final_sort.to_excel(writer, sheet_name=worksheet_name, index=False, startrow=1, startcol=0)
+                    sorted_data.to_excel(writer, sheet_name=worksheet_name, index=False, startrow=1, startcol=0)
 
             return JsonResponse({"message": "Files processed and sorted successfully."})
 
@@ -68,11 +60,22 @@ def basketball(request):
     
     return JsonResponse({"error": "Please upload a file."}, status=400)
 
-courts = ["PSA - McKinney 1", "PSA - McKinney 2", "PSA - McKinney 3", "PSA - McKinney 4",
-          "PSA - McKinney 5", "PSA - McKinney 6", "PSA - McKinney 7", "PSA - McKinney 8",
-          "PSA - Murphy 1", "PSA - Murphy 2", "PSA - Murphy 3", "PSA - Murphy 4",
-          "PSA - Murphy 5", "PSA - Murphy 6", "PSA - Murphy 7", "PSA - Murphy 8",
-          "PSA 1 - Blue 1", "PSA 1 - Blue 2", "PSA 1 - Blue 3", "PSA 1 - Blue 4",
-          "PSA 1 - Red 4", "PSA 1 - Red 5", "PSA 1 - Green 1",
-          "PSA 2 - Silver 1", "PSA 2 - Silver 2", "PSA 2 - Silver 3", "PSA 2 - Silver 4",
-          "PSA 2 - Silver 5", "PSA 2 - Silver 6", "PSA 2 - Silver 7", "PSA 2 - Silver 8"]
+def process_data(data, venue):
+    data_dict = {
+        "Date": data["Date"].to_list(),
+        "Start Time": data["Start Time"].to_list(),
+        "Venue": data["Venue"].to_list(),
+        "League": data["League"].to_list(),
+        "Home Team": data["Home Team"].to_list(),
+        "Away Team": data["Away Team"].to_list()
+    }
+
+    df = pd.DataFrame(data_dict)
+    df.insert(5, "Home Score", "")
+    df.insert(7, "Away Score", "")
+    df.insert(8, "Referee Signature", "")
+    filtered_data = df[df['Venue'] == venue]
+    court_sorted_data = filtered_data.sort_values(by=["Venue", "Date"])
+    final_sort = court_sorted_data.drop(columns=['Venue', 'Date'])
+
+    return final_sort
