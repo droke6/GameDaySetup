cf5b2c829a0546783c009bff57a8214109017e61
Daniel Roque <droque6@icloud.com>
2024-06-09 17:26:11 -0500

    css changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar";
 import '../styles/Home.css';
+import '../styles/buttons.css'
 import { useNavigate } from 'react-router-dom';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
 
 function Home() {
   const navigate = useNavigate();
@@ -9,10 +11,18 @@ function Home() {
       navigate(path)
   }
 
+  const handleLogout = () => {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
+    navigate('/login');
+  };
+
   return (
     <>
       <div className="top">
-        <Navbar />
+      <button className='sign-out-button' type="button" onClick={handleLogout} style={{ whiteSpace: 'nowrap' }}>
+          Sign Out
+        </button>
       </div>
       <div className="content">
         <h1>PSA Game Sorter</h1>
@@ -20,6 +30,11 @@ function Home() {
         <div className="buttons">
           <button className="game-button" onClick={() => navigateTo("/basketball")}>Basketball</button>
           <button className="game-button" onClick={() => navigateTo("/volleyball")}>Volleyball</button>
+        </div>
+        <div className='bottom'>
+            <button className='sign-out-button2' type="button" onClick={handleLogout} style={{ whiteSpace: 'nowrap' }}>
+                Sign Out
+            </button>
         </div>
       </div>
     </>
