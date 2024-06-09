0ac6217a2477f2bf9a6ccc124a817e3eab3a95a7
Daniel Roque <droque6@icloud.com>
2024-06-09 00:29:46 -0500

    frontend and backend commit

--- frontend/src/pages/Basketball.jsx
+++ frontend/src/pages/Basketball.jsx
@@ -36,7 +36,7 @@ const Basketball = () => {
                 const url = window.URL.createObjectURL(new Blob([response.data]));
                 const link = document.createElement('a');
                 link.href = url;
-                link.setAttribute('download', 'sorted_data.xlsx');
+                link.setAttribute('download', 'basketball_games.xlsx');
                 document.body.appendChild(link);
                 link.click();
                 link.remove();
