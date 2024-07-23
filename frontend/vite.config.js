fa050b531f4f2b4979bbc383c275a1568ec41440
Daniel Roque <droque6@icloud.com>
2024-07-23 14:56:51 -0500

    volleyball home update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-datepicker'],
-    },
-  },
-});
+  plugins: [react()],
+})
