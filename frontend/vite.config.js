ab1877d3161886460bbed4ba1915610ff72322e5
Daniel Roque <droque6@icloud.com>
2024-07-23 14:54:06 -0500

    volleyball home update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import cssModules from 'vite-plugin-css-modules';
 
 export default defineConfig({
   plugins: [
     react(),
-    cssModules() // Add the cssModules plugin
   ],
   build: {
     rollupOptions: {
