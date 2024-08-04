8f9cad83fc97d1aa0cc332b5648c44dc762e3acf
Daniel Roque <droque6@icloud.com>
2024-08-03 19:17:59 -0500

    core fixes

--- backend/gameSheets/core_format.py
+++ backend/gameSheets/core_format.py
@@ -113,9 +113,9 @@ def generate_core_game_sheet(worksheet, home_team, away_team, venue, date, time,
     facility_name = venue.split()[-2]   
     court = venue.split()[-1]
 
-    worksheet.merge_range('B1:M1', "CORE - REGULAR SEASON GAME SCORE SHEET", title_format)
-    worksheet.merge_range('B2:M2', f"← ← Home Team: {home_team}", game_format)
-    worksheet.merge_range('B18:M18', f"Away Team: {away_team} → →", game_format)
+    worksheet.merge_range('B1:N1', "CORE - REGULAR SEASON GAME SCORE SHEET", title_format)
+    worksheet.merge_range('B2:N2', f"← ← Home Team: {home_team}", game_format)
+    worksheet.merge_range('B18:N18', f"Away Team: {away_team} → →", game_format)
     worksheet.merge_range('A2:A7', f"Court: {court}", vertical_format)
     worksheet.merge_range('A10:A16', f"Facility: {facility_name}", vertical_format)
     worksheet.merge_range('A19:A24', f"Time: {time}", vertical_format)
